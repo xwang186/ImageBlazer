fa53a4978cd331003b3081ff8a319b90ffc4b141
dante1218 <ttkkyyz18@gmail.com>
2020-07-18 21:11:46 -0400

    initialize front-end framework

---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="80%"
+    outlined
+  >
+    <v-card-text 
+        class="d-flex justify-center align-center"
+        :class="{'margin-vertical': image == null}">
+      <div height="500">
+        <div
+            v-if="image != null">
+            <v-img @click="removeImage" :src="image"/>
+        </div>
+        <div v-else>
+          <input type="file" @change="uploadImage($event)">
+        </div>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+
+export default {
+    data: () => ({
+        loading:false,
+        image: null,
+    }),
+    methods: {
+        uploadImage(e) {
+            var files = e.target.files || e.dataTransfer.files;
+            if (!files.length) return;
+            this.renderImage(files[0]);
+        },
+        renderImage(file) {
+            var reader = new FileReader();
+
+            reader.onload = (e) => {
+                this.image = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        },
+        removeImage: function () {
+            this.image = null; 
+        }
+    }
+}
+</script>
+
+<style>
+.margin-vertical{
+    margin-top: 200px;
+    margin-bottom: 200px;
+}
+</style>
