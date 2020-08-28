1c283ef7b781dc1fd61fd035fef34815473caa62
yishi-lu <ttkkyyz18@gmail.com>
2020-08-28 09:50:18 -0400

    feat: add algorithm for image grey-scale

--- backend/images/views.py
+++ backend/images/views.py
@@ -1,5 +1,6 @@
 import os
 import base64
+import io
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 from django.shortcuts import render, HttpResponse
@@ -10,6 +11,9 @@ from rest_framework.response import Response
 from rest_framework import views
 from rest_framework import viewsets
 from rest_framework.parsers import MultiPartParser, FileUploadParser
+
+from PIL import Image
+import numpy as np
 
 # Create your views here.
 class ImageViewSet(viewsets.ViewSet):
@@ -23,17 +27,30 @@ class ImageViewSet(viewsets.ViewSet):
 
     @action(detail=False, methods=['POST'])
     def process(self, request, format=None):
+
+        # read uploaded image
         image = request.FILES['image']
         extension = image.name.split('.')[-1]
 
-        image_data = base64.b64encode(image.read()).decode('utf-8')
+        # algorthm to change image grey-scale
+        img = Image.open(io.BytesIO(image.read()))
+        pixels = img.load()
+
+        w, h = img.size
+        for i in range(w):
+            for j in range(h):
+                R = pixels[i, j][0]
+                G = pixels[i, j][1]
+                B = pixels[i, j][2]
+                gray = R*0.299 + G*0.587 + B*0.114
+                pixels[i, j] = (int(gray), int(gray), int(gray))
+        buffered = io.BytesIO()
+        img.save(buffered, format='png')
+        result_image = base64.b64encode(buffered.getvalue())
 
         # store image in local file
         # image_data = ContentFile(image.read())
-        # path = default_storage.save('tmp/test'+extension, image_data)
+        # path = default_storage.save('tmp/test.'+extension, image_data)
         # tmp_file = os.path.join(settings.MEDIA_ROOT, path)
-        print(settings.MEDIA_ROOT)
 
-        # print(image)
-        # print(image_data)
-        return HttpResponse(image_data, content_type='image/'+extension)
+        return HttpResponse(result_image, content_type='image/'+extension)
